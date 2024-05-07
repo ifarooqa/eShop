4fabf03d590be719f755de56d1765bb3692d9e8a
Pir Saib <farooqazam1432@gmail.com>
2024-05-08 00:38:19 +0500

    done with binding of images.

--- src/app/Components/product-listing/product-listing.component.ts
+++ src/app/Components/product-listing/product-listing.component.ts
@@ -18,10 +18,29 @@ export class ProductListingComponent {
       title: 'Product 1',
       description: 'first product description',
       price: 3.5,
+      imageUrl: 'assets/images/product1.png',
     },
-    { id: 2, title: 'Product 2', description: 'second product', price: 4 },
-    { id: 3, title: 'Product 3', description: 'third product', price: 2.5 },
-    { id: 4, title: 'Product 4', description: 'four product', price: 5.5 },
+    {
+      id: 2,
+      title: 'Product 2',
+      description: 'second product',
+      price: 4,
+      imageUrl: 'assets/images/product2.webp',
+    },
+    {
+      id: 3,
+      title: 'Product 3',
+      description: 'third product',
+      price: 2.5,
+      imageUrl: 'assets/images/product3.webp',
+    },
+    {
+      id: 4,
+      title: 'Product 4',
+      description: 'four product',
+      price: 5.5,
+      imageUrl: 'assets/images/product4.jpg',
+    },
   ];
 
   updateRating(product: any, step: number) {
